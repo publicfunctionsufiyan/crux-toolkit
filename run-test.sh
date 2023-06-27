2d0edd5e26d7e19976c389fd8c22a1b5575ddc9d
Attila <kfattila@yandex.com>
2023-06-27 12:08:06 +0300

    scripts updated

--- run-test.sh
+++ run-test.sh
@@ -16,18 +16,18 @@ TIDE_INDEX_PARAM="--max-mods 1 --mods-spec 1M+15.9949,1STY+79.966331,K+229.16293
 
 # Tide-search 
 OUTPUT_DIR=/blob/semi-correct-results
-TIDE_SEARCH_PARAM="--mz-bin-width 0.02 --precursor-window 10 --precursor-window-type ppm --concat F --top-match 1 --use-tailor-calibration T --num-threads 6 --exact-p-value F"
+TIDE_SEARCH_PARAM="--mz-bin-width 0.02 --precursor-window 100 --precursor-window-type ppm --concat F --top-match 1 --use-tailor-calibration T --num-threads 6 --exact-p-value F"
 
 
-# INDEX_DIR=/blob/HUMAN_NT_SHUFFLE_LEN_7-50_COMMON_MOD_1
-# FILEROOT=nt_shuf_sep_10ppm
-# $CRUX tide-search $TIDE_SEARCH_PARAM  --fileroot $FILEROOT --overwrite T --output-dir $OUTPUT_DIR /blob/dda/PXD017407/*.mzML $INDEX_DIR
-# $CRUX assign-confidence --score "tailor score" --fileroot $FILEROOT --overwrite T --output-dir $OUTPUT_DIR $OUTPUT_DIR/$FILEROOT.tide-search.target.txt
+INDEX_DIR=/blob/HUMAN_NT_SHUFFLE_LEN_7-50_COMMON_MOD_1
+FILEROOT=nt_shuf_sep_100ppm
+$CRUX tide-search $TIDE_SEARCH_PARAM  --fileroot $FILEROOT --overwrite T --output-dir $OUTPUT_DIR /blob/dda/PXD017407/*.mzML $INDEX_DIR
+$CRUX assign-confidence --score "tailor score" --fileroot $FILEROOT --overwrite T --output-dir $OUTPUT_DIR $OUTPUT_DIR/$FILEROOT.tide-search.target.txt
 
-# INDEX_DIR=/blob/HUMAN_NT_REVERSE_LEN_7-50_COMMON_MOD_1
-# FILEROOT=nt_rev_sep_10ppm
-# $CRUX tide-search $TIDE_SEARCH_PARAM  --fileroot $FILEROOT --overwrite T --output-dir $OUTPUT_DIR /blob/dda/PXD017407/*.mzML $INDEX_DIR
-# $CRUX assign-confidence --score "tailor score" --fileroot $FILEROOT --overwrite T --output-dir $OUTPUT_DIR $OUTPUT_DIR/$FILEROOT.tide-search.target.txt
+INDEX_DIR=/blob/HUMAN_NT_REVERSE_LEN_7-50_COMMON_MOD_1
+FILEROOT=nt_rev_sep_100ppm
+$CRUX tide-search $TIDE_SEARCH_PARAM  --fileroot $FILEROOT --overwrite T --output-dir $OUTPUT_DIR /blob/dda/PXD017407/*.mzML $INDEX_DIR
+$CRUX assign-confidence --score "tailor score" --fileroot $FILEROOT --overwrite T --output-dir $OUTPUT_DIR $OUTPUT_DIR/$FILEROOT.tide-search.target.txt
 
 
 # INDEX_DIR=/blob/HUMAN_FT_SHUFFLE_LEN_7-50_COMMON_MOD_1
@@ -88,12 +88,12 @@ TIDE_SEARCH_PARAM="--mz-bin-width 1.0005079 --precursor-window 100 --precursor-w
 # Use this to test (search) theoretical spectra against an index.
 TIDE_SEARCH_PARAM="--mz-bin-width 1.0005079 --precursor-window 100 --precursor-window-type ppm --concat F --top-match 2 --use-tailor-calibration F --num-threads 4 --exact-p-value F"
 
-INDEX_DIR=/blob/HUMAN_FT_REVERSE_LEN_7-50_MC3
-FILEROOT=ft_rev_sep_100ppm_theo_small
-$CRUX tide-search $TIDE_SEARCH_PARAM  --fileroot $FILEROOT --overwrite T --output-dir crux-output-theo theoretical_spectra.mgf $INDEX_DIR
-INDEX_DIR=/blob/HUMAN_FT_SHUFFLE_LEN_7-50_MC3
-FILEROOT=ft_shuf_sep_100ppm_theo_small
-$CRUX tide-search $TIDE_SEARCH_PARAM  --fileroot $FILEROOT --overwrite T --num-threads 16 --output-dir crux-output-theo theoretical_spectra.mgf $INDEX_DIR
+# INDEX_DIR=/blob/HUMAN_FT_REVERSE_LEN_7-50_MC3
+# FILEROOT=ft_rev_sep_100ppm_theo_small
+# $CRUX tide-search $TIDE_SEARCH_PARAM  --fileroot $FILEROOT --overwrite T --output-dir crux-output-theo theoretical_spectra.mgf $INDEX_DIR
+# INDEX_DIR=/blob/HUMAN_FT_SHUFFLE_LEN_7-50_MC3
+# FILEROOT=ft_shuf_sep_100ppm_theo_small
+# $CRUX tide-search $TIDE_SEARCH_PARAM  --fileroot $FILEROOT --overwrite T --num-threads 16 --output-dir crux-output-theo theoretical_spectra.mgf $INDEX_DIR
 
 
 
